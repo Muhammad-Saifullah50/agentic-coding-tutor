f0d52e54ea1605b96fd1124da4e86cb83577fb65
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2025-11-06 08:39:19 +0500

    feat: update CreateCoursePage to display a placeholder message instead of the component

--- frontend/src/app/(root)/courses/create/page.tsx
+++ frontend/src/app/(root)/courses/create/page.tsx
@@ -2,7 +2,10 @@ import CreateCourse from '@/components/CreateCourse'
 
 const CreateCoursePage = () => {
     return (
-        <CreateCourse />
+        // <CreateCourse />
+        <div className="flex items-center justify-center h-full">
+            <h1 className="text-2xl font-bold">Create Course Page - Coming Soon!</h1>
+        </div>
     )
 }
 
