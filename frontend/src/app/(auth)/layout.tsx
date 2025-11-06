e945b8489b1237029f0eb2ea0f3aa037481834d1
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2025-11-06 08:48:51 +0500

    feat: add AuthLayout component for authentication pages

---
+++ frontend/src/app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-screen w-full  flex items-center justify-center ">{children}</div>
+  )
+}
+
+export default AuthLayout
