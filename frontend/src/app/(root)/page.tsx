d70247be27a4b55e48bc01f3a022167f34df05e1
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2025-11-06 08:54:52 +0500

    refactor: remove user redirection logic from HomePage component

--- frontend/src/app/(root)/page.tsx
+++ frontend/src/app/(root)/page.tsx
@@ -4,14 +4,10 @@ import Features from "@/components/Features";
 import Pricing from "@/components/Pricing";
 import Footer from "@/components/Footer";
 import { getCurrentUser } from "@/actions/user.actions";
-import { redirect } from "next/navigation";
 
 const HomePage = async () => {
 
   const user = await getCurrentUser()
-  if (!user) {
-    redirect('/onboarding')
-  }
 
   return (
     <div className="min-h-screen">
